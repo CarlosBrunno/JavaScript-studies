6bbaba2495979cd0c2540d45f9c812d0fe99ad91
Carlos Brunno <carlos.brunno97@gmail.com>
2020-11-06 19:35:27 -0300

    Some care with numbers

---
+++ exercicios-js/fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)
+console.log("10" / 2)//Vale somente se o que estiver dentro da String for numero e sem vírgula.
+console.log("Show!" * 2)
+console.log(0.1 + 0.7) // Gera immprecisão.
+// console.log(10.toString())
+console.log((10.345).toFixed(2))
